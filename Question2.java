d4eaa42f2839221b877c9dfee3fa5c4b5700f048
wechsljm <wechsljm@miamioh.edu>
2017-02-23 15:51:30 -0500

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,8 @@
+
+public class Question2 {
+	
+	public static int returnBigger(int num1, int num2) {
+		return Math.max(num1, num2);
+	}
+
+}
---
+++ Question2Test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Question2Test {
+
+	@Test
+	public void testReturnBigger() {
+		assertEquals(2, Question2.returnBigger(2, 1));
+	}
+
+}
